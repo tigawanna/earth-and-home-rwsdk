eab25c7f7e6ab5945b2130b7908b97fd7a464d17
tigawanna <denniskinuthiawaweru@gmail.com>
2025-06-29 17:17:55 +0300

    feat: Add HeroSectionCaroussel component with initial structure

---
+++ src/app/components/home/HeroSectionCaroussel.tsx
@@ -0,0 +1,12 @@
+"use client"
+interface HeroSectionCarousselProps {
+
+}
+
+export function HeroSectionCaroussel({}:HeroSectionCarousselProps){
+return (
+ <div className='w-full h-full flex flex-col items-center justify-center'>
+
+ </div>
+);
+}
