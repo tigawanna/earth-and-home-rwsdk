240271561e82133ad9614c592287525f6af733d8
tigawanna <denniskinuthiawaweru@gmail.com>
2025-06-29 14:54:00 +0300

    feat: Import view transitions CSS in Document component for enhanced animations

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -1,11 +1,14 @@
 import styles from "./styles.css?url";
+import viewTransitions from "./view-transitions.css?url";
 
 export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <html lang="en"  data-style="wipe">
+  <html lang="en" data-style="wipe">
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="stylesheet" href={styles} />
+      <link rel="stylesheet" href={viewTransitions} />
+
       <title>Earth and home</title>
       <link rel="modulepreload" href="/src/client.tsx" />
     </head>
